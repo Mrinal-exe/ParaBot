caa2da4014d41ae09cd2c6ad862d3659c6d5f84d
Hasshhh <harshkr918@gmail.com>
2020-11-17 16:44:41 +0530

    Abcd

---
+++ commands/embed.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+module.exports.run = (bot,message,args)=>{   
+
+let content = args.slice(2).join(' ')
+
+const embedname = new Discord.MessageEmbed()
+.setDescription(content)
+.setColor('0x#00ffff')
+.setFooter(`REQUESTED BY ${message.author.tag}`,`${message.author.displayAvatarURL({dynamic : true})}`)
+.setTimestamp();
+message.channel.send(embedname)
+}
+
+module.exports.help ={
+    name:"embed"
+}
---
+++ commands/ping.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+module.exports.run = (bot,message,args)=>{
+
+    if(message.author.id != "685101975385276469") return message.reply(`You're Not A Owner ! `);
+
+    message.reply(`Pong!`);
+}
+
+module.exports.help ={
+    name:"ping"
+}
